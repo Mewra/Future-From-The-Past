80e0d8ed5cff860cfad88ab2be74a13ca223c0c2
Maura <maura.sacca@gmail.com>
2017-11-09 13:31:57 +0100

    Initial commit

---
+++ Assets/Scripts/MovingObstaclesDown.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingObstaclesDown: MonoBehaviour {
+
+	private TweenTransforms _ImTransf;
+	private bool isMoving=true;
+	private Transform _transform;
+	private IEnumerator coroutine;
+	private IEnumerator coroutine1;
+	private bool usable = true;
+
+	// Use this for initialization
+	void Start () {
+		_transform = GetComponent<Transform>();
+		_ImTransf = GetComponent<TweenTransforms> ();
+		_ImTransf.startingVector = _transform.position;
+		_ImTransf.endVector = new Vector3 (transform.position.x, transform.position.y - 5.0f, transform.position.z);
+		_ImTransf.Begin ();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(Input.GetKeyDown(KeyCode.Q)){
+			if (usable){
+				coroutine1 = Cooldown ();
+				StartCoroutine (coroutine1);
+				coroutine = StopTime ();
+				StartCoroutine (coroutine);
+
+
+				/*if (isMoving == true) {
+				_ImTransf.Pause ();
+				isMoving = false;
+			} else {
+				_ImTransf.Resume ();
+				isMoving = true;
+			}*/
+			}
+			usable = false;
+		}
+
+	}
+
+	private IEnumerator StopTime(){
+		_ImTransf.Pause ();
+		isMoving= false;
+		yield return new WaitForSeconds (2.0f);
+		_ImTransf.Resume ();
+		isMoving = true;
+	}
+
+	private IEnumerator Cooldown(){
+		yield return new WaitForSeconds (5.0f);
+		usable = true;
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Player : MonoBehaviour {
+
+	private float m_horizontal;
+	private float m_vertical;
+	public float m_speed = 150.0f;
+	private Transform _transform;
+	private Rigidbody2D _playerRb;
+	private float jumpPower = 0;
+	private bool isGrounded = true;
+	public RaycastHit2D hit;
+
+	// Use this for initialization
+	void Start () {
+		_transform = GetComponent<Transform>() as Transform;
+		_playerRb = GetComponent<Rigidbody2D> () as Rigidbody2D;
+		//_twtrans = GetComponent <TweenTransforms>;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		m_horizontal = Math.Sign (Input.GetAxis ("Horizontal"));
+		m_vertical = Math.Sign (Input.GetAxis ("Vertical"));
+		if (WorldTurn.getFuture ()) {
+			_transform.position = _transform.position +
+			_transform.right * m_horizontal * m_speed * Time.deltaTime +
+			_transform.up * m_vertical * m_speed * Time.deltaTime;
+
+		}else{
+			_transform.position = _transform.position + _transform.right * m_horizontal * m_speed * Time.deltaTime + 
+				_transform.up * m_vertical * jumpPower * Time.deltaTime;
+		}
+		
+	}
+
+	void OnCollisionEnter(Collision other)
+	{
+		if (other.transform.tag == "Ground") 
+			isGrounded = true;
+
+	}
+
+	void FixedUpdate(){
+		
+		if (Input.GetKeyDown (KeyCode.UpArrow)||Input.GetKeyDown(KeyCode.W) && isGrounded) {
+			isGrounded = false;
+			jumpPower = 7.5f;
+		}
+	}
+
+}
