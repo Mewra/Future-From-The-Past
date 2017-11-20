f237a3872f8718b0c96dbdf21e1a369e9c24a7d2
Unknown <maura.sacca@gmail.com>
2017-11-20 20:39:33 +0100

    Puzzle2 + tags, obstacles

---
+++ Assets/Scripts/OkeyScripts/ObFollowing.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObFollowing : MonoBehaviour {
+
+	public GameObject _target;
+	private Transform _transform;
+	private Transform _targetTransf;
+
+	// Use this for initialization
+	void Start () {
+		_transform = GetComponent<Transform>() as Transform;
+		_targetTransf = _target.GetComponent<Transform> () as Transform;
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		_transform.position = new Vector3 (_targetTransf.position.x, _transform.position.y,_transform.position.z);
+	}
+}
+
