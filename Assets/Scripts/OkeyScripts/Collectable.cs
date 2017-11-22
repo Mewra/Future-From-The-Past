6d32dc697c1556ba9029ca04ba85f3d17de1ff2c
Unknown <maura.sacca@gmail.com>
2017-11-22 21:06:27 +0100

    Level3 (collectables, timemigration, portals)

---
+++ Assets/Scripts/OkeyScripts/Collectable.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour {
+
+	public int _maxPieces;
+	private static int _piecesCollected = 0;
+	public GameObject _piece;
+	//private GameObject [] pieces = new GameObject [_maxPieces];
+	public GameObject _buildable;
+	private Transform _transformBuildable;
+	private string _tag;
+
+	// Use this for initialization
+	void Start () {
+
+		_transformBuildable = _buildable.GetComponent<Transform> ();
+		_tag = _buildable.tag;
+		_buildable.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter2D(Collision2D coll){
+		if (coll.gameObject.tag == "Player") {
+			_piecesCollected++;
+			if (_piecesCollected == _maxPieces) {
+				_buildable.SetActive (true);
+			}
+			_piece.SetActive (false);
+		}
+	}
+
+
+}
