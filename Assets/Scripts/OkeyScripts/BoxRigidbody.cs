bb8e4816673b6f4b4d4c0d1698c2fb3a70b8bd32
Unknown <maura.sacca@gmail.com>
2017-11-26 01:32:59 +0100

    Level2 Improve (Stop Time) + Prefabs (Background, Main Camera)

---
+++ Assets/Scripts/OkeyScripts/BoxRigidbody.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxRigidbody : MonoBehaviour {
+
+	private Rigidbody2D _RB;
+	// Use this for initialization
+	void Start () {
+		_RB = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void onCollisionEnter(Collision2D coll){
+		if (coll.gameObject.tag == "Player") {
+			_RB.WakeUp ();
+		}
+	}
+
+	void onCollisionExit(Collision2D coll){
+		if (coll.gameObject.tag == "Player") {
+			_RB.Sleep ();
+		}
+	}
+}
--- Assets/Scripts/OkeyScripts/ObFollowing.cs
+++ Assets/Scripts/OkeyScripts/ObFollowing.cs
@@ -9,6 +9,7 @@ public class ObFollowing : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		
 		_transform = GetComponent<Transform>() as Transform;
 		_targetTransf = _target.GetComponent<Transform> () as Transform;
 
@@ -16,7 +17,11 @@ public class ObFollowing : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		_transform.position = new Vector3 (_targetTransf.position.x, _transform.position.y,_transform.position.z);
+			_transform.position = new Vector3 (_targetTransf.position.x, _transform.position.y, _transform.position.z);
 	}
+
+
+
+
 }
 
