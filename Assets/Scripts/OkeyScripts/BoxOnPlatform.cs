b59d7987950fe4bacd57298da48e45d5f9969916
Unknown <maura.sacca@gmail.com>
2017-11-27 19:38:45 +0100

    Level 6 + improve lvl5

---
+++ Assets/Scripts/OkeyScripts/BoxOnPlatform.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxOnPlatform : MonoBehaviour {
+
+
+	private Transform _transform;
+	// Use this for initialization
+	void Start () {
+		_transform = GetComponent<Transform> ();
+
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+
+		
+	}
+
+    void OnCollisionStay2D(Collision2D coll)
+	{
+		if(coll.gameObject.tag == "MovingPlatform")
+		{
+			_transform.parent = coll.transform;
+		}
+	}
+
+	void OnCollisionExit2D(Collision2D coll)
+	{
+		if(coll.gameObject.tag == "MovingPlatform")
+		{
+			transform.parent = null;
+		}
+	}
+}
