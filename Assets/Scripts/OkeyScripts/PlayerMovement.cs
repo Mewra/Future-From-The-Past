4d0198e59b9492b6e9458ade1fac5a0ea15ad6c2
Unknown <cesareli1995@gmail.com>
2017-12-02 18:07:17 +0100

    Finished the animation part
    I also created a new Prefab called Player. It is the gameobject include the player, main camera, sound manager. I hope you like the changes I made.

--- Assets/Scripts/OkeyScripts/PlayerMovement.cs
+++ Assets/Scripts/OkeyScripts/PlayerMovement.cs
@@ -19,10 +19,12 @@ public class PlayerMovement : MonoBehaviour {
 	// This variable remain true when the player is in future
 
 	private Transform _transform;
+	private Animator _animator;
 	private Rigidbody2D _rigidbody;
 
 	private float nextTime=0.0f;
 	private SpriteRenderer _playerSpriteRenderer;
+
 
 	// Use this for initialization
 	void Start () {
@@ -30,8 +32,9 @@ public class PlayerMovement : MonoBehaviour {
 		InTheFuture=true;
 		// _animator = GetComponent<Animator>() as Animator;
 		_transform = GetComponent<Transform>() as Transform;
+		_animator = GetComponent<Animator>() as Animator;
 		_rigidbody = GetComponent<Rigidbody2D>() as Rigidbody2D;
-
+		_animator.SetInteger("FutureState",0);
 		// nextTime=0.0f;
 		// state = State.Idle;
 
@@ -40,18 +43,49 @@ public class PlayerMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {	
-		if(InTheFuture==true){
+		m_horizontal = Math.Sign(Input.GetAxis ("Horizontal"));
+		m_vertical = Math.Sign(Input.GetAxis ("Vertical"));	
 
-			m_horizontal = Math.Sign(Input.GetAxis ("Horizontal"));
-			m_vertical = Math.Sign(Input.GetAxis ("Vertical"));
+		
+
+		if(InTheFuture==true){
+			
+			if (Input.GetAxis("Horizontal")<0) {
+				Debug.Log("LEFT");
+				_animator.SetInteger("FutureState",1);
+			}else if (Input.GetAxis("Horizontal")>0) {
+				Debug.Log("RIGHT");
+				_animator.SetInteger("FutureState",2);
+			}else{
+				_animator.SetInteger("FutureState",0);
+			}
 
 			_transform.position = _transform.position +
 				_transform.right * m_horizontal * m_speed * Time.deltaTime +
 				_transform.up * m_vertical * m_speed * Time.deltaTime;
-		}else{
-
-			m_horizontal = Math.Sign(Input.GetAxis ("Horizontal"));
-			m_vertical = Math.Sign(Input.GetAxis ("Vertical"));		
+		}else{	
+			// if (Input.GetAxis("Vertical")<0){
+			// 	Debug.Log(Input.GetAxis("Vertical"));
+			// 	_animator.SetInteger("PastState",2);
+			// }else if (Input.GetAxis("Horizontal")<0) {
+			// 	_animator.SetInteger("PastState",1);
+			// }else if (Input.GetAxis("Horizontal")>0) {
+			// 	_animator.SetInteger("PastState",3);
+			// }else{
+			// 	_animator.SetInteger("FutureState",0);
+			// }
+			if (m_vertical<0){
+				Debug.Log(m_vertical);
+				_animator.SetInteger("PastState",2);
+			}else{
+				if(Input.GetAxis("Horizontal")==0){
+					_animator.SetInteger("PastState",0);
+				}else if (Input.GetAxis("Horizontal")<0) {
+					_animator.SetInteger("PastState",1);
+				}else if (Input.GetAxis("Horizontal")>0) {
+					_animator.SetInteger("PastState",3);
+				}
+			}
 
 			_transform.position = _transform.position +
 				_transform.right * m_horizontal * m_speed * Time.deltaTime +
@@ -68,10 +102,12 @@ public class PlayerMovement : MonoBehaviour {
 
 			if(InTheFuture==true){
 				_rigidbody.gravityScale=0;
+				_animator.SetTrigger("TravelToFuture");
 				SoundManager.Instance.PastTravelToFuture();
 			}
 			else {
 				_rigidbody.gravityScale=-1;
+				_animator.SetTrigger("TravelToPast");
 				SoundManager.Instance.FutureTravelToPast();
 			}
 				
