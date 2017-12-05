dac4680878ead2e9386311d322b5244261b4e74b
Unknown <maura.sacca@gmail.com>
2017-12-05 21:15:42 +0100

    Add the Spawn Condition + Some Tutorials + Graphics

--- Assets/Scripts/OkeyScripts/PlayerMovement.cs
+++ Assets/Scripts/OkeyScripts/PlayerMovement.cs
@@ -25,6 +25,9 @@ public class PlayerMovement : MonoBehaviour {
 	private float nextTime=0.0f;
 	private SpriteRenderer _playerSpriteRenderer;
 
+	public Transform _ResidueTransform;
+	private Collider2D [] obj;
+
 
 	// Use this for initialization
 	void Start () {
@@ -51,10 +54,10 @@ public class PlayerMovement : MonoBehaviour {
 		if(InTheFuture==true){
 			
 			if (Input.GetAxis("Horizontal")<0) {
-				Debug.Log("LEFT");
+				//Debug.Log("LEFT");
 				_animator.SetInteger("FutureState",1);
 			}else if (Input.GetAxis("Horizontal")>0) {
-				Debug.Log("RIGHT");
+				//Debug.Log("RIGHT");
 				_animator.SetInteger("FutureState",2);
 			}else{
 				_animator.SetInteger("FutureState",0);
@@ -75,7 +78,7 @@ public class PlayerMovement : MonoBehaviour {
 			// 	_animator.SetInteger("FutureState",0);
 			// }
 			if (m_vertical<0){
-				Debug.Log(m_vertical);
+				//Debug.Log(m_vertical);
 				_animator.SetInteger("PastState",2);
 			}else{
 				if(Input.GetAxis("Horizontal")==0){
@@ -91,37 +94,44 @@ public class PlayerMovement : MonoBehaviour {
 				_transform.right * m_horizontal * m_speed * Time.deltaTime +
 				_transform.up * m_vertical * m_jumpHeight * Time.deltaTime;
 		}
-		
-		if (Input.GetKeyDown(KeyCode.J))
-		{
-			// Debug.Log(nextTime+"!@#");
-			nextTime=Time.time+1.0f;
-			// Debug.Log("TIME TRAVEL! next time is "+nextTime+" now Time is: "+Time.time);
 
-			InTheFuture=!InTheFuture;
+		//Debug.Log("Can turn? " + PlayerResidue.GetCantTurn ());
 
-			if(InTheFuture==true){
-				_rigidbody.gravityScale=0;
-				_animator.SetTrigger("TravelToFuture");
-				SoundManager.Instance.PastTravelToFuture();
-			}
-			else {
-				_rigidbody.gravityScale=-1;
-				_animator.SetTrigger("TravelToPast");
-				SoundManager.Instance.FutureTravelToPast();
-			}
+		if (Input.GetKeyDown (KeyCode.J)) {
+
+			obj = Physics2D.OverlapCircleAll (_ResidueTransform.position, 0.4f);
+
+			if (obj.Length == 1) {
+
+				// Debug.Log(nextTime+"!@#");
+				nextTime = Time.time + 1.0f;
+				// Debug.Log("TIME TRAVEL! next time is "+nextTime+" now Time is: "+Time.time);
+
+				InTheFuture = !InTheFuture;
+
+				if (InTheFuture == true) {
+					_rigidbody.gravityScale = 0;
+					_animator.SetTrigger ("TravelToFuture");
+					SoundManager.Instance.PastTravelToFuture ();
+				} else {
+					_rigidbody.gravityScale = -1;
+					_animator.SetTrigger ("TravelToPast");
+					SoundManager.Instance.FutureTravelToPast ();
+				}
 				
-			SoundManager.Instance.SwitchBGM1(InTheFuture);
-			_rigidbody.Sleep();
+				SoundManager.Instance.SwitchBGM1 (InTheFuture);
+				_rigidbody.Sleep ();
 
-			Vector3 playerPositionNow = _transform.position;
-			_transform.position = new Vector3(playerPositionNow.x, -playerPositionNow.y, playerPositionNow.z);
-			//_transform.Rotate (0, 180, 180);
-			bool aa = _playerSpriteRenderer.flipY;
+				Vector3 playerPositionNow = _transform.position;
+				_transform.position = new Vector3 (playerPositionNow.x, -playerPositionNow.y, playerPositionNow.z);
+				//_transform.Rotate (0, 180, 180);
+				bool aa = _playerSpriteRenderer.flipY;
 
 			
-			// Press J to Switch to another gravity System
+				// Press J to Switch to another gravity System
+			}
 		}
+
 
 		if ((Time.time>nextTime)&&(nextTime!=0.0f)){
 			SoundManager.Instance.SwitchBGM2(InTheFuture);
---
+++ Assets/Scripts/OkeyScripts/Reset.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reset : MonoBehaviour {
+
+	private Transform _transform;
+
+	private Vector3 _initialPosition;
+	private Quaternion _initialRotation;
+	private Vector3 _initialScale;
+
+
+	// Use this for initialization
+	void Start () {
+		_transform = GetComponent<Transform> ();
+		
+	}
+
+	void Awake(){
+		_transform = GetComponent<Transform> ();
+		_initialPosition = _transform.position;
+		_initialRotation = _transform.rotation;
+		_initialScale = _transform.localScale;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//Reset this object to his initial position
+		if (Input.GetKeyDown (KeyCode.R)) {
+			_transform.position = _initialPosition;
+			_transform.rotation = _initialRotation;
+		}
+		
+	}
+
+
+}
