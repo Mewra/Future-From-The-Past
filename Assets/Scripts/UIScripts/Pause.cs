844e53bc39e5c3c99d35dd46990ed657619b6eec
Unknown <cesareli1995@gmail.com>
2018-01-03 00:55:49 +0100

    Ingame puase UI finished
    Check it in the first level. For the other levels I have not done. But I have done the prefab. Now I just need more levels.

---
+++ Assets/Scripts/UIScripts/Pause.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Pause : MonoBehaviour {
+
+	public GameObject _UIIngame;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	void Awake()
+    {
+        Button button = GetComponent<Button>() as Button;
+        button.onClick.AddListener(btClick);
+    }
+
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void btClick()
+    {
+        print("Button Click");
+        _UIIngame.SetActive (true);
+    }
+}
---
+++ Assets/Scripts/UIScripts/Restart.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Restart : MonoBehaviour {
+
+		void Start () {
+		}
+
+		void Awake()
+		{
+			Button button = GetComponent<Button>() as Button;
+			button.onClick.AddListener(btClick);
+		}
+
+
+		void Update () {
+		}
+
+		void btClick()
+		{
+			SceneManager.LoadScene(ButtonLoadingScripts.levelNum);
+		}
+}
